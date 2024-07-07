7d9c987705e2cd22f677756c8e17300515e14f4e
User <user@Users-MacBook-Air.local>
2024-07-07 19:28:59 +0530

    server code updated

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-const cors = require("cors");
-const authRoutes = require("./routes/auth")
-const employee = require("./routes/employee")
-// require("./conn/conn");
-app.use(express.json());
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.use('/uploads', express.static('uploads'))
-app.use(cors());
-
-app.use("/admin",authRoutes)
-app.use("/employee",employee)
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-// connect to server
-app.listen(port, () => {
-    console.log(`server listening at http://localhost:${port}`)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const cors = require("cors");
+// const authRoutes = require("./routes/auth")
+// const employee = require("./routes/employee")
+// // require("./conn/conn");
+// app.use(express.json());
+
+// app.use('/uploads', express.static('uploads'))
+// app.use(cors());
+
+// app.use("/admin",authRoutes)
+// app.use("/employee",employee)
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+// });
+
+// // connect to server
+// app.listen(port, () => {
+//     console.log(`server listening at http://localhost:${port}`)
+// })
